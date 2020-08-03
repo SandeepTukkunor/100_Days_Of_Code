e8c37fd04c1dc0491c33dc6dd350e0afd8a4b46b
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-08-03 23:59:39 +0530

    py for everybody

---
+++ Data Science/Python for everybody/Programing for evrybody/week 3/week3.py
@@ -0,0 +1,19 @@
+# basic math and assignmnet of stateements 
+x = 3
+x = x +3 # 3 will be added to its previous valuse
+print(x)
+
+
+
+print(5-6)
+print(5+6)
+print(5*6)
+print(5/6)
+
+# in week 3, it was all about basic aritametic and undrstanding how the code is written , how indentations are give etc.
+
+n = 5 
+while n >0:
+    print(n)
+    n = n-1
+print("balstoff")
