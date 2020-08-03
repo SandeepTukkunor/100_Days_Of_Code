1d9521082d1822627bb95c8999543877d4361f62
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-08-04 00:12:49 +0530

    expressions

---
+++ Data Science/Python for everybody/Programing for evrybody/week 4/expressions.py
@@ -0,0 +1,13 @@
+#  constants : fixed values such as numbers , strings, letters etc
+print("hello world")
+print(98.5)
+
+
+
+
+# reserved words
+# the words we must use exact. Ex if, else, while, for def etc
+  
+
+# variables;  place where we ask python to allocate bit memory 
+#ex  x = 2  y = 5 etc
