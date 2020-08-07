fbe2744c5f38dad632e62893cf4a0c4c54766c90
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-08-07 17:54:03 +0530

    py for everybody

---
+++ Data Science/Python for everybody/Programing for evrybody/week 4/functions.py
@@ -0,0 +1,34 @@
+x = 5
+print("hello world")
+
+
+
+def print_lyrics():
+    print("Re re Bajarangi")
+    print("wo bulati hai magar jane ka nahi")
+
+print("yo")
+print_lyrics()
+x = x +2
+
+print(x)
+
+
+#adding two numbers 
+def add(n1, n2):
+    return n1 + n2 
+
+sum1 = add(1,3)
+print(sum1)
+
+
+# greeting 
+def greet(lang):
+    if lang == "es":
+        print("hola")
+    elif lang == "fr":
+        print("Banjur")
+    else:
+        print("hello")
+
+greet("eg")
