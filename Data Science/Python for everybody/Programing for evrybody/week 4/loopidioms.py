5e7bda3c5e8cdcd9965068633a97fdee74c084bf
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-08-08 11:06:40 +0530

    py for everybody

--- Data Science/Python for everybody/Programing for evrybody/week 4/loopidioms.py
+++ Data Science/Python for everybody/Programing for evrybody/week 4/loopidioms.py
@@ -25,9 +25,18 @@ print("the sum of all digits  is" ,tot_sum)
 #finding the averge in loop 
 
 tot_sum = 0 
-
+count = 0
 for i in [9, 41,13,3,74 , 15]:
+    count = count +1
+    tot_sum = tot_sum + i 
+    averge = tot_sum/ count
     
+
+    print(count,averge,  tot_sum)
+
+print("the average of list is:" ,tot_sum/count)
+
+   
 
 
 
