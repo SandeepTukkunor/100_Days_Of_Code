a69f3a2499cbbd77d78e242258b0e481f97b0933
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-08-09 10:40:49 +0530

    py4e chapter6

---
+++ Data Science/Python for everybody/Data Structure/week 3/chapter 7.1.py
@@ -0,0 +1,15 @@
+# first we have to open a file
+#fhand = open("mbox-short.txt", ) # making file available for python 
+#print(fhand)
+
+
+stuff = "hello \nworld"
+print(stuff)
+
+"""
+file = open("mbox-short.txt")
+for cheese in file:
+    print(cheese)
+
+    # prints all the lines from text 
+"""
