e6d1b6bba3e89a8257c9c6c7a59d39d4151e7ac5
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-08-09 11:51:18 +0530

    py4e chapter6

--- Data Science/Python for everybody/Data Structure/week 3/chapter 7.1.py
+++ Data Science/Python for everybody/Data Structure/week 3/chapter 7.1.py
@@ -13,3 +13,48 @@ for cheese in file:
 
     # prints all the lines from text 
 """
+
+
+file = open("mbox-short.txt")
+for line in file:
+    line = line.strip()  # removes the space which the following line creates 
+    if line.startswith("From:"):
+        print(line)
+
+
+# skippinf with continue
+file = open("mbox-short.txt")
+for line in file:
+    line = line.strip()
+    if not line.startswith("From:"):
+        continue
+    print(line)
+
+
+
+#using "in" to select lines 
+file = open("mbox-short.txt")
+for line in file:
+    line = line.strip()
+    if not "@uct.ac.za" in line:
+        continue
+    print(line)
+
+
+#dealing with bad names 
+file = input("please enter ur file name: ")
+
+try:
+    fileopen = open(file)
+except:
+    print("please enter a valid file name and check for the directory in case you have lost", file)
+
+count = 0
+
+for line in fileopen:
+    if line.startswith("Subject:"):
+        count = count+1
+print("there were ", count, "subject lines ", file)
+    
+
+
