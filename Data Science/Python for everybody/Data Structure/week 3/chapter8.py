43e7609f9519139d8e351e663d5c6d68b98d9ddc
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-08-09 15:09:22 +0530

    py4e chapter6

---
+++ Data Science/Python for everybody/Data Structure/week 3/chapter8.py
@@ -0,0 +1,74 @@
+lis = [1,2,3,4,5]
+for i in lis:
+    print(i)
+
+print("Blastoff")
+
+#strings are immutable and lists are mutable
+lotto = [1,5,6,3,4,8]
+# if we want to change 5 to 9
+lotto[1] = 9
+print(lotto)
+
+
+
+#range function 
+print(list(range(5)))
+
+friends = ["Sandeep" , "sunil", "Anil"]
+print(len(friends))
+print(list(range(len(friends))))
+
+
+
+
+# Manupulating lists 
+friends = ["Sandeep" , "sunil", "Anil"]
+for friend in friends:
+    print("Happy new year", friend)
+
+
+# slicing works siilar to string 
+#concnatenating also work similar to strings
+
+
+#building a list from scratch 
+
+stuff = list()
+stuff.append("book")
+stuff.append(99)
+stuff.append("cookie")
+print(stuff)
+
+
+#is somthing in the list
+some = [1,3,4,2,6,7,3,4,6]
+print(9 in some)
+
+print(min(some)) #to find the minimum
+print(max(some))
+
+"""
+total = 0 
+count = 0 
+
+while True:
+    inp = input("please enter a number")
+    if inp == "done":
+        break
+    Value = float(inp)
+    total = total + Value
+    count = count+1
+print("Average", total/count)
+"""
+
+numlist = list()
+while True:
+    inp = input("please enete a number")
+    if inp == "Done":
+        break
+    value = float(inp)
+    numlist.append(value)
+print("The average is", sum(numlist)/ len(numlist))
+
+
