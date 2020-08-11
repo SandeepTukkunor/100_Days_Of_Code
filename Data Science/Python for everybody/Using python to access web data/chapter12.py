1162b1054eaa9382ca28e43287b51a51b05b951d
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-08-11 21:57:42 +0530

    py for everybody

--- Data Science/Python for everybody/Using python to access web data/chapter12.py
+++ Data Science/Python for everybody/Using python to access web data/chapter12.py
@@ -1,3 +1,4 @@
+"""
 import socket
 mysoc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysoc.connect(("data.pr4e.org", 80))
@@ -12,3 +13,25 @@ while True:
     print(data.decode())
 
 mysoc.close() 
+"""
+
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
+
+
+
+
+
+
