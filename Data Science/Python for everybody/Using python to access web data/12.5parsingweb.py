ddf1b57e5285479663298d04e4228ced77aebcc7
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-08-12 23:49:43 +0530

    py for everybody

--- Data Science/Python for everybody/Using python to access web data/12.5parsingweb.py
+++ Data Science/Python for everybody/Using python to access web data/12.5parsingweb.py
@@ -1,5 +1,6 @@
 #scrapping the web 
 #insatll beautiful soupe 
+"""
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
@@ -22,6 +23,65 @@ tags = soup("a")
 
 for tag in tags:
     print(tag.get("href", None))
+"""
 
 
 # pip install --upgrade html5lib==1.0b8, This has solved the error
+
+
+
+
+"""
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+spans = soup('span')
+numbers = []
+
+for span in spans:
+    numbers.append(int(span.string))
+
+print (sum(numbers))
+"""
+
+
+
+
+import urllib.request as ur
+from bs4 import *
+
+current_repeat_count = 0
+url = "http://py4e-data.dr-chuck.net/known_by_Kylian.html"
+repeat_count = int(input('Enter count: '))
+position = int(input('Enter position: '))
+
+
+def parse_html(url):
+    html = ur.urlopen(url).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    return tags
+
+while current_repeat_count < repeat_count:
+    print('Retrieving: ', url)
+    tags = parse_html(url)
+    for index, item in enumerate(tags):
+        if index == position - 1:
+            url = item.get('href', None)
+            name = item.contents[0]
+            break
+        else:
+            continue
+    current_repeat_count += 1
+print('Last Url: ', url)
--- Data Science/Python for everybody/Using python to access web data/lesson12.3.py
+++ Data Science/Python for everybody/Using python to access web data/lesson12.3.py
@@ -13,6 +13,7 @@ for line in fhand:
 
 """
 
+"""
 import urllib.request, urllib.parse, urllib.error
 
 fhand = urllib.request.urlopen("http://data.pr4e.org/romeo.txt")
@@ -23,3 +24,19 @@ for line in fhand:
     for word in words: 
         counts[word] = counts.get(word,0) + 1
 print(counts)
+"""
+
+
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen("http://py4e-data.dr-chuck.net/known_by_Kylian.html")
+
+counts = dict()
+for line in fhand:
+    words = line.decode().split() # strip will give alphabets 
+    for word in words: 
+        counts[word] = counts.get(word,0) + 1
+print(counts)
+
+
+
