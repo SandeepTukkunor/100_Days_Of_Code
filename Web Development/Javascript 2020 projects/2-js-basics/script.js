7336c77ca6db97306f06e75de717f8552ecd7e5b
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-09-13 13:53:41 +0530

    math files added

--- Web Development/Javascript 2020 projects/2-js-basics/script.js
+++ Web Development/Javascript 2020 projects/2-js-basics/script.js
@@ -1,1 +1,62 @@
-console.log("Hello world");
+/*
+
+var firstname = "Sandeep";
+console.log(firstname);
+
+var lastname = "Tukkunor";
+var age = 23;
+
+var fullAge = true;
+console.log(fullAge);
+
+
+var job;
+console.log(job);
+//these are the variables 
+/*
+this is how multiline commenting is done 
+
+job = "teacher";
+console.log(job) // will taje var job as input  
+
+
+
+
+
+
+// variable mutation and type correction 
+
+var firstname = "Sandeep";
+var age = 28;
+
+console.log(firstname + " " , age);
+
+
+var job, isMarried;
+
+job = "teacher";
+isMarried = false;
+
+console.log(firstname + " is a " + age + " years old " + job + " is he a married? " + isMarried );
+
+
+// variable mutatipns
+
+age = 'twenty eight';
+job = "student"
+
+
+//alert(firstname + " is a " + age + " years old " + job + " is he a married? " + isMarried );
+
+
+var lastname = prompt("what is your last name?")
+console.log(firstname + " " + lastname)
+
+*/
+
+//basic math operators
+
+
+
+
+
