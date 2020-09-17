654a91ac0acaf6ceb3270d6fdd668c9ca16bd63d
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-09-17 20:51:12 +0530

    exercise

--- Web Development/Javascript 2020 projects/2-js-basics/script.js
+++ Web Development/Javascript 2020 projects/2-js-basics/script.js
@@ -359,13 +359,141 @@ var whatDoYouDo = function(job, firstname){
 console.log(whatDoYouDo("designer", "sandeep")) 
 
 
-*/
+
 
 // Arrays: collection of variables
 var names = ["sandeep", "sunil", "Anil"];
 var years = new Array(1997, 1999, 2001);
 console.log(names[0])
-console.log(names[1])
+console.log(names.length);
+names[1] = "bhagyashree"
+console.log(names)
+names[names.length] = "Sunil"
+console.log(names)
+
+
+
+
+// different data types in arrays 
+
+var john = ["john", "smith", 1990, "teacher", false];
+john.push("blue"); // ads at the end 
+john.unshift("mr"); // adds at the beginning 
+john.pop() // removest the element from the end
+john.shift() // removes the first element of the array 
+console.log(john.indexOf(1990))
+console.log(john);
+
+
+var isDesigner = john.indexOf("designer") === -1 ? "john is not a designer ": "john is a designer"
+console.log(isDesigner)
+
+
+
+
+
+// coding challenge : tip calculator
+
+
+function tipCalculator(bill) {
+    var percentage;
+    if (bill < 50) {
+
+        percentage = 0.2;
+    } else if (bill > 50 && bill < 200){
+        percentage = 0.15;
+    } else {
+        percentage = 0.1;
+    }
+    return percentage * bill
+}
+
+var bills = [124, 48, 268];
+var tips = [tipCalculator(bills[0]), tipCalculator(bills[1]), tipCalculator(bills[2])];
+console.log(tips)
+
+
+
+// * Objects and prorperties ** 
+
+var john = {
+    firstName :"John", 
+    lastName : "Smith", 
+    birthYear : 1990,
+    family: ["jane", "mark", "bob", "Emily"],
+    job : "teacher", 
+    isMarried : false
+};
+
+console.log(john.firstName)
+console.log(john["lastName"])
+
+
+john.job = "designer";
+john.isMarried = true;
+
+console.log(john)
+
+
+
+var jane  = new Object()
+jane.lastName = "morris";
+jane.birthYear = 1998;
+jane.job = "web developer";
+jane.isMarried = true;
+console.log(jane)
+
+ 
+
+
+// Methods
+
+var john = {
+    firstName :"John", 
+    lastName : "Smith", 
+    birthYear : 1990,
+    family: ["jane", "mark", "bob", "Emily"],
+    job : "teacher", 
+    isMarried : false,
+    calcAge: function (birthYear) {
+        return 2018 - this.birthYear;
+    }
+};
+
+john.age = john.calcAge()
+console.log(john)
+
+
+*/
+
+// coading challenge objects and methods 
+
+var john = {
+    fullname : "john smith",
+    mass : 92,
+    height : 1.95,
+    calcBMI : function(){
+        this.bmi = this.mass / (this.height* this.height)
+        return this.bmi
+    }
+}
+
+
+var mark = {
+    fullname : "Mark Miler",
+    mass : 78,
+    height : 1.69,
+    calcBMI : function(){
+        this.bmi = this.mass / (this.height* this.height)
+        return this.bmi
+    }
+}
+
+
+
+john.calcBMI();
+mark.calcBMI();
+console.log(john, mark)
 
 
 
