28a2be3fc6afbd31f38aaa4e23d45d71c8ad38c3
SandeepTukkunor <sandeepbidar1997@gmail.com>
2020-12-19 11:18:19 +0530

    tableau coures started

---
+++ Data Science/12 Beginner python projects(freecodecamp)/Madlibs/madlibs.py
@@ -0,0 +1,16 @@
+"""
+#string concatination 
+youtuber = "Sandeep"
+#few ways are as follows
+print("please subscribe to "+ youtuber)
+print("plese subscribe to {}".format(youtuber))
+print(f"please subscribe to {youtuber}")
+
+"""
+
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("famous person: ")
+madlib = f"computer programming is so {adj}! It makes me so exited all the times because i love to {verb1}. Stay hydrated and {verb2} like yoy are {famous_person}"
+print(madlib)
