7817ca9e6fda145aca9e45dd08494837bfb4fdb3
SandeepTukkunor <sandeepbidar1997@gmail.com>
2021-01-06 16:44:46 +0530

    second project completed

---
+++ Data Science/12 Beginner python projects(freecodecamp)/Guess the number(user)/guess_the_number(user).py
@@ -0,0 +1,44 @@
+import random 
+
+def guess(x):
+    random_num = random.randint(1,x)
+    guess = 0
+    while guess != random_num:
+
+        guess = int(input(f"please enter a number between 1 and {x}"))
+        if guess < random_num:
+            print("sorry Guess again, the value is too low")
+
+        elif guess > random_num:
+            print("the number is too high, Guess again")
+        
+    print("yey! you have guessed it correctly , Congrats")
+
+
+def computer_guess(x):
+    low = 1
+    high=x
+
+    feedback = ""
+    while feedback != "c":
+        if low != high:
+
+            guess = random.randint(low, high)
+            
+        else:
+            guess = low 
+
+        feedback =input(f"is {guess}  too high(H) or too low(L) or just correct(C)")
+        if feedback == "h":
+            high = guess-1
+
+        elif feedback == 'l':
+            low = guess + 1
+    print(f"yay, the computer predicted the {guess} correctly ")
+
+computer_guess(10)
+        
+
+
+
+
