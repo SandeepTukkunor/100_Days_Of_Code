737d7d5bd40642dcf03bdb667d6d0a126131701a
SandeepTukkunor <sandeepbidar1997@gmail.com>
2021-02-03 12:48:15 +0530

    video basics started

--- Data Science/Open CV(Udemy)/Numpy And Image Basics/opening_image_by_script.py
+++
@@ -1,9 +0,0 @@
-import cv2
-img = cv2.imread("C:/Users/Sandeep Tukkunor/Desktop/Udemy Notes/Computer-Vision-with-Python/DATA/00-puppy.jpg")
-
-while True:
-    cv2.imshow("puppy", img)
-    #if we waited atleast one millisecond and pressed thee esc
-    if cv2.waitKey(10000):
-        break
-cv2.destroyAllWindows()
