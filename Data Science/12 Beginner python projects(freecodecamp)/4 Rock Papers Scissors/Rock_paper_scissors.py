8f5b988984fd291e3ca1acf04050192ea03173c6
SandeepTukkunor <sandeepbidar1997@gmail.com>
2021-02-18 13:46:12 +0530

    rock papers scissors

--- Data Science/12 Beginner python projects(freecodecamp)/4 Rock Papers Scissors/Rock_paper_scissors.py
+++ Data Science/12 Beginner python projects(freecodecamp)/4 Rock Papers Scissors/Rock_paper_scissors.py
@@ -3,3 +3,27 @@ import random
 def play():
     user = input(" 'r' for rock, 'p' for paper , 's' for scissor")
     computer = random.choice(["r", "p","s"])
+
+
+    if user == computer:
+        print("tie")
+
+    if is_win(user, computer):
+        return "you won "
+    
+    return "you lost "
+
+#r> s, s>p, p>r
+
+
+def is_win(player, oppenent):
+
+    if (player == 'r' and oppenent == "s") or (player == 's' and oppenent == 'p' ) or (player == 'p' and oppenent == 'r'):
+        return "you won"
+
+print(play())
+
+
+
+
+
