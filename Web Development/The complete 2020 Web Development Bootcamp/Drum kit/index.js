abbc0bb62311676df6b2e5790de8da443bb7647f
SandeepTukkunor <sandeepbidar1997@gmail.com>
2021-07-23 00:28:24 +0530

    java script

--- Web Development/The complete 2020 Web Development Bootcamp/Drum kit/index.js
+++ Web Development/The complete 2020 Web Development Bootcamp/Drum kit/index.js
@@ -1,20 +1,66 @@
 
+var numberOFDrums = document.querySelectorAll(".drum").length;
 
-var numberOfButtons = document.querySelectorAll(".drum").length
+for (var i = 0; i<numberOFDrums;i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case 'w':
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+            
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+
+            case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+
+            case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
 
 
-for (var i =0; i < numberOfButtons;i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+            case "j":
+                var kickbass = new Audio("sounds/kick-bass.mp3");
+                kickbass.play();
+                break;
+
+            case "k":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
 
 
+            
 
 
-        // var audio = new Audio("sounds/tom-1.mp3");
-        // audio.play()
-        // alert("i got clicked")
+            case "l":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
 
-        console.log(this.style.color = "white")
+
+            
+
+            
+
+                
+
+
+                
+                
         
+            default: console.log(buttonInnerHTML);
+
+                
+        }
+    
     });
 
 
@@ -22,8 +68,7 @@ for (var i =0; i < numberOfButtons;i++){
 }
 
 
-
-
-
-
+// var audio = new Audio('sounds/crash.mp3');
+//         audio.play();
+        // 
 
