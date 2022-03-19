2070701ffb54181eae256c80154a05858462a07f
SandeepTukkunor <sandeepbidar1997@gmail.com>
2022-03-19 16:15:41 +0530

    added new files

--- Web Development/The complete 2020 Web Development Bootcamp/Drum kit/index.js
+++ Web Development/The complete 2020 Web Development Bootcamp/Drum kit/index.js
@@ -4,62 +4,10 @@ var numberOFDrums = document.querySelectorAll(".drum").length;
 for (var i = 0; i<numberOFDrums;i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
         var buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML) {
-            case 'w':
-                var tom1 = new Audio('sounds/tom-1.mp3');
-                tom1.play();
-                break;
-            
-            case "a":
-                var tom2 = new Audio("sounds/tom-2.mp3");
-                tom2.play();
-                break;
-
-            case "s":
-                var tom3 = new Audio("sounds/tom-3.mp3");
-                tom3.play();
-                break;
-
-            case "d":
-                var tom4 = new Audio("sounds/tom-4.mp3");
-                tom4.play();
-                break;
-
-
-            case "j":
-                var kickbass = new Audio("sounds/kick-bass.mp3");
-                kickbass.play();
-                break;
-
-            case "k":
-                var crash = new Audio("sounds/crash.mp3");
-                crash.play();
-                break;
-
-
-            
-
-
-            case "l":
-                var snare = new Audio("sounds/snare.mp3");
-                snare.play();
-                break;
-
-
-            
-
-            
-
-                
-
-
-                
-                
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
         
-            default: console.log(buttonInnerHTML);
-
-                
-        }
+        
     
     });
 
@@ -67,8 +15,72 @@ for (var i = 0; i<numberOFDrums;i++) {
 
 }
 
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+    
+});
 
-// var audio = new Audio('sounds/crash.mp3');
-//         audio.play();
-        // 
+function makeSound(key){
+    switch (key) {
+        case 'w':
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+
+        case "j":
+            var kickbass = new Audio("sounds/kick-bass.mp3");
+            kickbass.play();
+            break;
+
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+
+        
+
+
+        case "l":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        
+        default: console.log(buttonInnerHTML);
+
+
+
+            
+    }
+
+}
+
+function buttonAnimation(currentKey){
+    
+    var activeButton = document.querySelector("." + currentKey)
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+
+    },100)
+
+}
 
---
+++ Web Development/The complete 2020 Web Development Bootcamp/jquery/index.js
@@ -0,0 +1,4 @@
+
+$("button").click(function() {
+    $("h1").animate({opacity: 0.5});
+});
